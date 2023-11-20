4e9f4b131ad96182e79ca0ba1f99cf60d2ac64ea
Julian Rich <123442863+ric394@users.noreply.github.com>
2023-11-20 15:49:16 +1000

    Start of observedReport implementation

---
+++ ApsimNG/Presenters/ObservedReportPresenter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UserInterface.Presenters;
+
+namespace ApsimNG.Presenters
+{
+    public class ObservedReportPresenter : IPresenter
+    {
+        public void Attach(object model, object view, ExplorerPresenter explorerPresenter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Detach()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
